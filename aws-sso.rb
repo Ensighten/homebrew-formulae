725536922f8d1e964881d5795e78e4246906c6a6
Scott Cunningham <scott.cunningham@ensighten.com>
2016-03-04 13:12:32 +0100

    Add SHA256s for aws-sso py deps

--- aws-sso.rb
+++ aws-sso.rb
@@ -17,16 +17,17 @@ class AwsSso < Formula
   end
 
   py_deps = %w[
-    r/requests/requests-2.7.0
-    b/beautifulsoup4/beautifulsoup4-4.4.1
-    b/boto/boto-2.38.0
-    P/PyYAML/PyYAML-3.11
-    l/lxml/lxml-3.5.0
+    r/requests/requests-2.7.0 398a3db6d61899d25fd4a06c6ca12051b0ce171d705decd7ed5511517b4bb93d
+    b/beautifulsoup4/beautifulsoup4-4.4.1 87d4013d0625d4789a4f56b8d79a04d5ce6db1152bb65f1d39744f7709a366b4
+    b/boto/boto-2.38.0 d9083f91e21df850c813b38358dc83df16d7f253180a1344ecfedce24213ecf2
+    P/PyYAML/PyYAML-3.11 c36c938a872e5ff494938b33b14aaa156cb439ec67548fcab3535bb78b0846e8
+    l/lxml/lxml-3.5.0 349f93e3a4b09cc59418854ab8013d027d246757c51744bf20069bc89016f578
   ]
 
-  py_deps.each do |x|
+  py_deps.each_slice(2) do |x, y|
     resource x do
       url "https://pypi.python.org/packages/source/#{x}.tar.gz"
+      sha256 "#{y}"
     end
   end
 

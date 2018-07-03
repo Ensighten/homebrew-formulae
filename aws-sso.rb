4b76f9d23b9b22f76191d0cd49d756501c106ab4
Joel Baranick <joel.baranick@ensighten.com>
2018-07-03 12:08:23 -0700

    Fix homepage

--- aws-sso.rb
+++ aws-sso.rb
@@ -1,6 +1,6 @@
 class AwsSso < Formula
   desc "Tool to generate temporary AWS credentials using Okta SSO"
-  homepage "https://github.com/kadaan/homebrew-aws-sso/homebrew-aws-sso.git"
+  homepage "https://github.com/homebrew-formulae/homebrew-aws-sso/homebrew-aws-sso.git"
 
   url "https://github.com/Ensighten/homebrew-formulae/releases/download/untagged-6bea8cff89968f4c92fb/aws-profile_1.5.0_mac.tbz2"
   sha256 "966ee546b68cd97323dde3117c99607f06463a0ead0c70c88b871eca1bb4bb00"

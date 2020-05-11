abe8c1dc86a02d1c7678a34f487567d260d8de9c
Joel Baranick <kadaan@users.noreply.github.com>
2020-05-11 10:22:10 -0700

    Update to aws-sso 1.5.7

--- aws-sso.rb
+++ aws-sso.rb
@@ -2,9 +2,9 @@ class AwsSso < Formula
   desc "Tool to generate temporary AWS credentials using Okta SSO"
   homepage "https://github.com/homebrew-formulae/homebrew-aws-sso/homebrew-aws-sso.git"
 
-  url "https://github.com/Ensighten/homebrew-formulae/releases/download/v1.5.6/aws-profile_1.5.6_mac.tbz2"
-  sha256 "0843453f62a2323dadd024813509ec555279dfd0e52363de2e0c1173a2ad9712"
-  version "1.5.6"
+  url "https://github.com/Ensighten/homebrew-formulae/releases/download/v1.5.7/aws-profile_1.5.7_mac.tbz2"
+  sha256 "646620608adeeebba4a17ef7e33e6338d5f791c358b2837dc61db0728e786482"
+  version "1.5.7"
 
   bottle :unneeded
 

6e3a7b11c89fe7013c417022e0d1a10a70ab0df0
Joel Baranick <kadaan@users.noreply.github.com>
2020-05-14 10:29:36 -0700

    Create dumb-init.rb

---
+++ dumb-init.rb
@@ -0,0 +1,18 @@
+class AwsSso < Formula
+  desc "A minimal init system for Linux containers"
+  homepage "https://github.com/Yelp/dumb-init"
+
+  url "https://github.com/Ensighten/homebrew-formulae/releases/download/v1.2.2.dumbinit/dumb-init_1.2.2_mac.bz2"
+  sha256 "cf5c9f4f9d586e7952bf3ae5fb6f643cc568cdec939a56525ddc5f4f25ea7cb5"
+  version "1.2.2"
+
+  bottle :unneeded
+
+  def install
+    bin.install "dumb-init"
+  end
+
+  test do
+    system "aws-profile", "--help"
+  end
+end

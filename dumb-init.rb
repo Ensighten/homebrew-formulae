a6e313ef00621d8380271c08b18ea7182386caaf
Joel Baranick <kadaan@users.noreply.github.com>
2020-05-14 11:53:34 -0700

    Update dumb-init.rb

--- dumb-init.rb
+++ dumb-init.rb
@@ -2,8 +2,8 @@ class DumbInit < Formula
   desc "A simple process supervisor that forwards signals to children."
   homepage "https://github.com/Yelp/dumb-init"
 
-  url "https://github.com/Ensighten/homebrew-formulae/releases/download/v1.2.2.dumbinit/dumb-init_1.2.2_mac.bz2"
-  sha256 "cf5c9f4f9d586e7952bf3ae5fb6f643cc568cdec939a56525ddc5f4f25ea7cb5"
+  url "https://github.com/Ensighten/homebrew-formulae/releases/download/v1.2.2.dumbinit/dumb-init_1.2.2_mac.tar.bz2"
+  sha256 "4a3fc83f690156bdb1d80d3e181583f02265dfbb1f2c41935d79842ce0c4af90"
   version "1.2.2"
 
   bottle :unneeded

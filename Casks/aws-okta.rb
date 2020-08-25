4d365fa913e86ad20adad05b00590e8094c162d1
Joel Baranick <kadaan@users.noreply.github.com>
2020-08-25 15:10:26 -0700

    Update aws-okta.rb

--- Casks/aws-okta.rb
+++ Casks/aws-okta.rb
@@ -1,6 +1,6 @@
 cask 'aws-okta' do
-  version '0.0.13'
-  sha256 'e0a2482261f065fc18c33394d69342ebb929e0c66f37338246e851c7dd874075'
+  version '0.0.14'
+  sha256 '05d0fde9bc4d5112559699b8cd54b35fb8c03720efc367e875965194dd3ff879'
 
   url "https://github.com/Ensighten/homebrew-formulae/releases/download/v#{version}.aws-okta/aws-okta-darwin-amd64.dmg"
   name 'aws-okta'

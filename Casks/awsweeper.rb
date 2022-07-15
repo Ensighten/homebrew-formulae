cf9d6aa844778f1ffa15ec5713b47f8950c9a17f
Joel Baranick <kadaan@users.noreply.github.com>
2022-07-15 01:36:36 -0700

    Update awsweeper.rb

--- Casks/awsweeper.rb
+++ Casks/awsweeper.rb
@@ -1,6 +1,6 @@
 cask 'awsweeper' do
   version '0.0.13'
-  sha256 '760e611083b3e6f8477c762dfe0e47c8452257467ff38de8ccb699b1ebd824f0'
+  sha256 'cd953029c8cc3cbf585639feabce07e1a64143e41f3547ad7e97ef6dfb16d19a'
 
   url "https://github.com/Ensighten/homebrew-formulae/releases/download/v#{version}.awsweeper/awsweeper-darwin.dmg"
   name 'awsweeper'

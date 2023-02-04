6221f8d9a9b86f8d56cdd4738da566b3a7dc93a5
Joel Baranick <kadaan@users.noreply.github.com>
2023-02-03 18:35:24 -0800

    Update ansible-jinja2-tester.rb

--- Casks/ansible-jinja2-tester.rb
+++ Casks/ansible-jinja2-tester.rb
@@ -1,6 +1,6 @@
 cask 'ansible-jinja2-tester' do
-    version '0.0.1'
-    sha256 'ddde0d28e00b28c098b936f17b2d886653c570bfeac3db6204d7af44506d240b'
+    version '0.0.2'
+    sha256 '8b6499228f8e00566556e41431bb66c357bbb9f63096b3586ee13b855d474ae5'
   
     url "https://github.com/Ensighten/homebrew-formulae/releases/download/v#{version}.ansible-jinja2-tester/Ansible.Jinja2.Tester.dmg"
     name 'Ansible Jinja2 Tester'

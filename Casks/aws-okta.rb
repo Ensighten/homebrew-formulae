3dee6111d25f65153545932b1a62e4271c58e546
Joel Baranick <kadaan@users.noreply.github.com>
2024-02-01 17:32:07 -0800

    Update aws-okta.rb

--- Casks/aws-okta.rb
+++ Casks/aws-okta.rb
@@ -1,4 +1,6 @@
 cask 'aws-okta' do 
+  arch arm: "arm", intel: "intel"
+  
   version '0.0.28'
   sha256 arm:   'e86345ca2a187ac895a8706f6ae650bed11a5876611e937b5345cf2d54a467de',
          intel: '1cfbd1aa4b305e4ecb1f0b92e704e37a78ba7351ff9b61aae7916fcf57956e7c'

7c63b3fb2ac6b61d9745eefe0712617b6ba00e02
Joel Baranick <kadaan@users.noreply.github.com>
2024-03-08 15:18:33 -0800

    Update devbox.rb

--- Casks/devbox.rb
+++ Casks/devbox.rb
@@ -1,11 +1,8 @@
 cask 'devbox' do 
-  arch arm: "arm", intel: "intel"
-  
-  version '0.91.1-ens001'
-  sha256 arm:   '1521c7d9137d84f5260704d48e662d3f385dcc2fa8069abe7b7ece2962d9ed74',
-         intel: '66e79bdfced1a0b804d7ce478607bf3dafeffe1bc1ea4f3a7bf5c8b385ee2613'
+  version '0.9.1-ens001'
+  sha256 '7649ccc4d40c1b79adaf3b5604e04bd6b2b9b2043d686badaffff7122f4e0f32'
 
-  url "https://github.com/Ensighten/homebrew-formulae/releases/download/v#{version}.devbox/devbox-darwin-#{arch}.tar.gz"
+  url "https://github.com/Ensighten/homebrew-formulae/releases/download/v#{version}.devbox/devbox-darwin.dmg"
   name 'devbox'
   homepage 'https://github.com/kadaan/devbox'
 

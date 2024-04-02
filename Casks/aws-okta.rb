f78a150f565f00c299819a8b5289400dcc85dd38
Joel Baranick <kadaan@users.noreply.github.com>
2024-04-02 08:45:02 -0700

    Update aws-okta.rb

--- Casks/aws-okta.rb
+++ Casks/aws-okta.rb
@@ -1,9 +1,9 @@
 cask 'aws-okta' do 
   arch arm: "arm", intel: "intel"
   
-  version '0.0.40'
-  sha256 arm:   '885aaaf078ac9af2599402ffa67f1edb8848e4867ed3b182c7606f8f8b0d20c4',
-         intel: '9cc26caf7f1eba0648e8b780e00b814161e88554e6df3a222d571986619447a0'
+  version '0.0.41'
+  sha256 arm:   '4dc0c410677314132c37aef6d6bb1b1429ea9314189f42ff816b2db3a90948b5',
+         intel: '385a9a20da8bdd695cd040473d5912d0ef2005c548edb49ba12ea06d78e9dea0'
 
   url "https://github.com/Ensighten/homebrew-formulae/releases/download/v#{version}.aws-okta/aws-okta-darwin-#{arch}.dmg"
   name 'aws-okta'

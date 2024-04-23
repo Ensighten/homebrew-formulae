2c9f75433f0bdf5509f731f9c37cfabd546f23e8
Joel Baranick <kadaan@users.noreply.github.com>
2024-04-23 10:25:58 -0700

    Update dumb-init.rb

--- dumb-init.rb
+++ dumb-init.rb
@@ -2,9 +2,13 @@ class DumbInit < Formula
   desc "A simple process supervisor that forwards signals to children."
   homepage "https://github.com/Yelp/dumb-init"
 
-  url "https://github.com/Ensighten/homebrew-formulae/releases/download/v1.2.2.dumbinit/dumb-init_1.2.2_mac.tar.bz2"
-  sha256 "4a3fc83f690156bdb1d80d3e181583f02265dfbb1f2c41935d79842ce0c4af90"
-  version "1.2.2"
+  arch arm: "arm", intel: "intel"
+
+  version "1.2.5"
+  sha256 arm:   '8ece8d16f74474481043a6dbaff8296c3968fcf13d7729b75330c7333eaa398a',
+         intel: 'fc256f98ca53ae52acda2e8ec806a08794ffb41676fdeb41cb3c8ea51f1d7a6b'
+
+  url "https://github.com/Ensighten/homebrew-formulae/releases/tag/v#{version}.dumb-init/dumb-init-darwin-#{arch}.tar.bz2"
 
   def install
     bin.install "dumb-init"

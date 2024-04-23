1d127cc8f2caefee4f13c6ceb724af4dd370ad14
Joel Baranick <kadaan@users.noreply.github.com>
2024-04-23 11:43:23 -0700

    Update dumb-init.rb

--- dumb-init.rb
+++ dumb-init.rb
@@ -3,7 +3,7 @@ class DumbInit < Formula
   homepage "https://github.com/Yelp/dumb-init"
 
   version "1.2.5"
-  url "https://github.com/Ensighten/homebrew-formulae/releases/tag/v#{version}.dumb-init/dumb-init.tar.bz2"
+  url "https://github.com/Ensighten/homebrew-formulae/releases/download/v#{version}.dumb-init/dumb-init.tar.bz2"
   sha256 "44a414847309518a690dc9d86cfe8dd75788cfe23f3baa9d89088d0a89b0be72"
   
   def install

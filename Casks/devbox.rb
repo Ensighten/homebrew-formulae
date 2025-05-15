a6f9adef8b8c4b2bedb89cc78f287a76d344fb51
Joel Baranick <kadaan@users.noreply.github.com>
2025-05-15 14:16:16 -0700

    Update devbox.rb

--- Casks/devbox.rb
+++ Casks/devbox.rb
@@ -1,6 +1,6 @@
 cask 'devbox' do 
-  version '0.10.1-ens001'
-  sha256 'dff4d7222717ab09d0e072f8220db460dd9e8c0e224829fe8c1af16fa33eb500'
+  version '0.14.2-ens001'
+  sha256 '08054e17b13cb0eb08cde1b59999568e82e93416be6decf013958d0faa17c5f6'
 
   url "https://github.com/Ensighten/homebrew-formulae/releases/download/v#{version}.devbox/devbox-darwin.dmg"
   name 'devbox'
